87a736da5b12c8826f1e333c4017972fda3777c1
Evgenij <zena936872.0@gmail.com>
2023-09-25 19:12:58 +0300

    ДЗ от 25.09

--- Django/advertisements/app_advertisements/models.py
+++ Django/advertisements/app_advertisements/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
 
 # Create your models here.
 class Advertisement(models.Model): 
@@ -9,8 +12,22 @@ class Advertisement(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date()==timezone.now().date():
+            created_time=self.created_at.time().strftime('%H:%M:%S')
+            return format_html('<span style="color: green; font-weight: bold">Сегодня в {}</span>', created_time)
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+    
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.updated_at.date()==timezone.now().date():
+            updated_time=self.updated_at.time().strftime('%H:%M:%S')
+            return format_html('<span style="color: pink; font-weight: bold">Сегодня в {}</span>', updated_time)
+        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+
     def __str__(self):
-        return f'{self.title}, {self.description}, {self.price}, {self.auction}, {self.created_at}, {self.updated_at}'
+        return f'Advertisement(id={self.id}, title={self.title}, description={self.description}, price={self.price})'
 
     class Meta():
         db_table='advertisements'
