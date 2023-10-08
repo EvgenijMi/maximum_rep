7251a1a0bf5d83f7581b6e7fb4e788a9e1ec164d
Evgenij <zena936872.0@gmail.com>
2023-10-08 19:26:23 +0300

    ДЗ

--- Django/advertisements/app_advertisements/views.py
+++ Django/advertisements/app_advertisements/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from .models import Advertisement
 from .forms import AdvertisementForms
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def index(request):
     advertisements=Advertisement.objects.all()
     context={'advertisements':advertisements}
-    return render(request, 'index.html', context)
+    return render(request, 'app_advertisements/index.html', context)
 
 def top_sellers(request):
-    return render(request, 'top-sellers.html')
+    return render(request, 'app_advertisements/top-sellers.html')
 
+@login_required(login_url=reverse_lazy('login'))
 def adv_post(request):
     if request.method=="POST":
         form=AdvertisementForms(request.POST, request.FILES)
@@ -26,4 +28,4 @@ def adv_post(request):
     else:
         form=AdvertisementForms()
     context={'form': form}
-    return render(request, 'advertisement-post.html')
+    return render(request, 'app_advertisements/advertisement-post.html', context)
